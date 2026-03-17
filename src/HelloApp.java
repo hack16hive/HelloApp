aae188ae054c69612237968745be5091a528397e
hack16hive <hritika1602@gmail.com>
2026-03-17 09:16:21 +0530

    Initial commit

---
+++ src/HelloApp.java
@@ -0,0 +1,26 @@
+/**
+ * HelloApp.java- a simple Java application that prints "Hello, World!" to the console.
+ * This is the first step in the HelloApp journey, where we start with a basic
+ * "hello!, world!" program. The application consists of a single class with a main method that serves as the entry point of the program. When executed, it will display the message "Hello, World!" in the console.
+ * This is a fundamental example of a Java application and serves as a starting point for learning Java programming. It demonstrates the basic structure of a Java program, including the class definition and the main method, which is essential for any Java application to run.
+ * 
+ * Author: Hritika Singh
+ * @version 1.0
+ */
+
+/**
+ * Key Java Concept Used:
+ * 1. Class declaration: Defines a blueprint for objects.
+ * 2. Main method: The entry point of program execution.
+ * 3. Static Keyword: Indicates that the method belongs to the class, not an instance.
+ * 4. String Argument Array: Parameters passed to the main method.
+ * 5. System.out.println: A method used to print messages to the console.
+ */
+
+public class HelloApp 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+}
